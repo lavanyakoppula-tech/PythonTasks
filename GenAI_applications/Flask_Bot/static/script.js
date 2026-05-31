e196430cbb21194912543b627723410216bd4b2a
lavanya <lavanyakoppula29@gmail.com>
2026-05-31 14:49:39 +0530

    commit

---
+++ GenAI_applications/Flask_Bot/static/script.js
@@ -0,0 +1,46 @@
+async function sendMessage(){
+
+    let input =
+        document.getElementById("user-input");
+
+    let message =
+        input.value;
+
+    if(message === "")
+        return;
+
+    let chatBox =
+        document.getElementById("chat-box");
+
+    chatBox.innerHTML +=
+        "<div class='user'><b>You:</b> "
+        + message +
+        "</div>";
+
+    input.value = "";
+
+    const response =
+        await fetch("/chat",{
+
+        method:"POST",
+
+        headers:{
+            "Content-Type":"application/json"
+        },
+
+        body:JSON.stringify({
+            message:message
+        })
+    });
+
+    const data =
+        await response.json();
+
+    chatBox.innerHTML +=
+        "<div class='bot'><b>Bot:</b> "
+        + data.response +
+        "</div>";
+
+    chatBox.scrollTop =
+        chatBox.scrollHeight;
+}
